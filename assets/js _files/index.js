9cd1f72d8fd4cb5b0ed654c9211cfa4fa7600169
sean-code <john.nganga@student.moringaschool.com>
2022-07-17 19:06:56 +0300

    add part one of the function in JavaScript

--- assets/js _files/index.js
+++ assets/js _files/index.js
@@ -1,1 +1,10 @@
-alert("My code is working");
+// alert("My code is working");
+
+//PART ONE
+var shed = ["A 510", "B 308", "C 486", "D 572"];
+for (var i = 0; i < shed.length; i++) {
+
+    console.log("Your production in shed " + shed[i] + " litres per day.");
+}
+
+
