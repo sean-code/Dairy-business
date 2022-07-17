2b24cc6cdf07fe95dc0be9660548ea921110182e
sean-code <john.nganga@student.moringaschool.com>
2022-07-17 19:15:17 +0300

    add code to evaluate for the total production per day

--- assets/js _files/index.js
+++ assets/js _files/index.js
@@ -6,5 +6,7 @@ for (var i = 0; i < shed.length; i++) {
 
     console.log("Your production in shed " + shed[i] + " litres per day.");
 }
-
+var A = 510; var B = 308; var C = 486; var D = 572;
+var totalProduction = (A+B+C+D);
+     console.log("The total production is " + totalProduction + " litres per day");
 
